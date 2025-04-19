f6fc2e77c9728643510944c41a1a93de25577f86
bqsdsg <guomy97@gmail.com>
2025-04-19 18:24:24 +0800

    feat: add analyze router to main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routers import chat
 from routers import predict, record
+from routers import analyze
 
 app = FastAPI()
 app.include_router(predict.router)
 app.include_router(chat.router)
+app.include_router(analyze.router)
